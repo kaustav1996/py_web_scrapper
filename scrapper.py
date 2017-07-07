ffcf5ea20295ec3b5c9e4adeff7bd6d07e7de312
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-07-07 12:48:31 -0400

    scrapper almost complete
    scrapper complete.. only creating a csv file with the outputs is left

--- scrapper.py
+++ scrapper.py
@@ -3,9 +3,10 @@ import urllib
 from bs4 import BeautifulSoup
 mainsite="http://www.thelearningpoint.net"
 url="http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school"
-urls =[url] #stack of urls to scrape
+urls =[url,"http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=10","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=20","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=30","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=40","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=50","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=60","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=70","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=80","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=90"] #stack of urls to scrape
 schools=list()
 names=list()
+emails=list()
 while len(urls)>0:
 	htmltext=urllib.urlopen(urls[0]).read()
 	soup= BeautifulSoup(htmltext,"lxml")
@@ -18,9 +19,33 @@ print("\n".join(schools))
 while len(schools)>0:
 	htmltext=urllib.urlopen(schools[0]).read()
 	soup= BeautifulSoup(htmltext,"lxml")
-	schools.pop(0)
-	ele=soup.find('span',{'id': 'sites-page-title'})
-	names.append(ele.text.partition(":")[0])
-	ele=soup.find('td',{'class': 'sites-layout-tile sites-tile-name-content-1'}).find('b')
-	print ele
 	
+	ele=soup.find('span',{'id': 'sites-page-title'})
+	nm=ele.text.partition(":")[0]
+	if(nm==''):#if Name is not found no need to scrape for email
+		print("Parsing Failed!! No Name Found at "+schools[0])
+	elif nm.find("Schools")!=-1 or nm.find("Top")!=-1 :
+		print("Parsing Failed!! No Name Found at " +schools[0])
+	else:#if Name is found trying to scrape email
+		names.append(nm)
+		for ele in soup.find('td',{'class': 'sites-layout-tile sites-tile-name-content-1'}).findAll('b'):
+			flag=0
+			if ele.text.find("Email:")!=-1:
+				em=ele.text[ele.text.find("Email:")+6:ele.text.find(",")]
+				if em=='':
+					em="No Email Found"#if "Email:" is present but no email is given
+				emails.append(em)
+				flag=1
+				break
+		if(flag==0):
+			print("Parsing Failed!! No Email Found for "+nm) #if Email: is not present
+			em="No Email Found"
+			emails.append(em)
+	schools.pop(0)
+i=-1
+while(i< len(names)):
+	if(i==-1):
+		print("""School No."""+","+"""School Name"""+"""Email""")
+	else:
+		print(('"{}"'+","+'"{}"'+","+'"{}"').format(str(i+1),names[i],emails[i]))
+	i=i+1
