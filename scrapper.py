e36583383ec0952a49f117f13be26f3673b2c2ba
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-07-07 14:29:25 -0400

    Add files via upload

--- scrapper.py
+++ scrapper.py
@@ -1,22 +1,22 @@
 import urlparse
 import urllib
+import csv
 from bs4 import BeautifulSoup
 mainsite="http://www.thelearningpoint.net"
 url="http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school"
 urls =[url,"http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=10","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=20","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=30","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=40","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=50","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=60","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=70","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=80","http://www.thelearningpoint.net/system/app/pages/search?scope=search-site&q=school&offset=90"] #stack of urls to scrape
-schools=list()
-names=list()
-emails=list()
-while len(urls)>0:
+schools=list()#list of links that r to be parsed
+names=list()#names of schools parsed from the headlines of the links
+emails=list()#emails of the schools parsed from body
+print("Collecting URLs, please wait.. ")
+while len(urls)>0: #getting all the urls that are needed to be parsed
 	htmltext=urllib.urlopen(urls[0]).read()
 	soup= BeautifulSoup(htmltext,"lxml")
 	urls.pop(0)
 	for tag in soup.find('div',{'class': 'sites-search-results-wrapper'}).findAll('a',href= True):
 		tag['href'] = mainsite+tag['href']
 		schools.append(tag['href'])
-
-print("\n".join(schools))
-while len(schools)>0:
+while len(schools)>0: #parsing starts
 	htmltext=urllib.urlopen(schools[0]).read()
 	soup= BeautifulSoup(htmltext,"lxml")
 	
@@ -41,11 +41,13 @@ while len(schools)>0:
 			print("Parsing Failed!! No Email Found for "+nm) #if Email: is not present
 			em="No Email Found"
 			emails.append(em)
-	schools.pop(0)
+	schools.pop(0) #parsing ends for the url and popped it out from the stack
 i=-1
-while(i< len(names)):
-	if(i==-1):
-		print("""School No."""+","+"""School Name"""+"""Email""")
-	else:
-		print(('"{}"'+","+'"{}"'+","+'"{}"').format(str(i+1),names[i],emails[i]))
-	i=i+1
+with open('school_info.csv','wb') as csvfile:
+	wr=csv.writer(csvfile,delimiter=',')
+	while(i< len(names)):
+		if(i==-1):
+			wr.writerow(['No.']+['School Name']+['Email'])
+		else:
+			wr.writerow([str(i+1)]+[names[i]]+[emails[i]])
+		i=i+1
